ef247fc51b6512590ff6a65757489577adfa08a3
hyeonwook98 <hyeonwook98@naver.com>
2023-05-22 17:30:43 +0900

    fix: 빌더 패턴 제거 후 수정

---
+++ src/main/java/com/sodamdadam/server/domain/user/service/UserLoginService.java
@@ -0,0 +1,49 @@
+package com.sodamdadam.server.domain.user.service;
+
+import com.sodamdadam.server.domain.user.dto.request.UserLoginRequest;
+import com.sodamdadam.server.domain.user.dto.response.UserLoginResponse;
+import com.sodamdadam.server.domain.user.entity.User;
+import com.sodamdadam.server.domain.user.repository.UserRepository;
+import com.sodamdadam.server.global.dto.response.CommonResponse;
+import com.sodamdadam.server.global.error.ErrorCode;
+import com.sodamdadam.server.global.exception.SodamDadamException;
+import com.sodamdadam.server.global.jwt.JwtProperties;
+import com.sodamdadam.server.global.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UserLoginService {
+
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Transactional
+    public ResponseEntity<CommonResponse> loginUser(UserLoginRequest request) {
+
+        User findUser = userRepository.findById(request.getId())
+                .orElseThrow(() -> new SodamDadamException(ErrorCode.USER_NOT_FOUND));
+
+        if (!passwordEncoder.matches(request.getPassword(), findUser.getPassword())) {
+            throw new SodamDadamException(ErrorCode.USER_NOT_FOUND);
+        }
+
+        String accessToken = jwtTokenProvider.createToken(request.getId());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(CommonResponse.builder()
+                        .status(HttpStatus.OK.value())
+                        .message("UserLogin Success")
+                        .data(new UserLoginResponse(JwtProperties.TOKEN_PREFIX +accessToken))
+                        .build());
+
+    }
+}
