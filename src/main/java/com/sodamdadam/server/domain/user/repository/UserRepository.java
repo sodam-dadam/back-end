eae9479b4114a537ede894885584e037d327958d
hyeonwook98 <hyeonwook98@naver.com>
2023-05-25 23:27:02 +0900

    fix: UserRepository 인터페이스 메서드 수정

--- src/main/java/com/sodamdadam/server/domain/user/repository/UserRepository.java
+++ src/main/java/com/sodamdadam/server/domain/user/repository/UserRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByIdAndPassword(String id, String password);
+    Optional<User> findById(String id);
 }
