15ac27e5c7ef9339ea6d7c5db4ab424b42cf4016
hyeonwook98 <hyeonwook98@naver.com>
2023-06-13 21:41:07 +0900

    feat: jwt 토큰 리프래쉬 토큰 메서드 추가

--- src/main/java/com/sodamdadam/server/global/jwt/JwtTokenProvider.java
+++ src/main/java/com/sodamdadam/server/global/jwt/JwtTokenProvider.java
@@ -5,24 +5,52 @@ import com.sodamdadam.server.global.exception.SodamDadamException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+@RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
 
-    public String createToken(String id) {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public String createAccessToken(String id) {
 
         return Jwts.builder()
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
+                .setExpiration(new Date(System.currentTimeMillis() + JwtProperties.ACCESS_EXPIRATION_TIME))
                 .setSubject(id)
                 .claim("id",id)
                 .signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encodeToString(JwtProperties.SECRET_KEY.getBytes()))
                 .compact();
+
+    }
+
+    public String createRefreshToken(String id) {
+
+        String refreshToken = Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + JwtProperties.REFRESH_EXPIRATION_TIME))
+                .setSubject(id)
+                .claim("id", id)
+                .signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encodeToString(JwtProperties.SECRET_KEY.getBytes()))
+                .compact();
+
+        // redis에 저장
+        redisTemplate.opsForValue().set(
+                id,
+                refreshToken,
+                JwtProperties.REFRESH_EXPIRATION_TIME,
+                TimeUnit.MILLISECONDS
+        );
+
+        return refreshToken;
 
     }
 
