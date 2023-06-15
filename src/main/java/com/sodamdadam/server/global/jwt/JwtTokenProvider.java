4a13c0f3546c9df14c4fb0b3352f67075273acfe
hyeonwook98 <hyeonwook98@naver.com>
2023-06-15 23:26:05 +0900

    feat: 엑세스 토큰 조작 여부 확인 메서드 추가

--- src/main/java/com/sodamdadam/server/global/jwt/JwtTokenProvider.java
+++ src/main/java/com/sodamdadam/server/global/jwt/JwtTokenProvider.java
@@ -2,9 +2,7 @@ package com.sodamdadam.server.global.jwt;
 
 import com.sodamdadam.server.global.error.ErrorCode;
 import com.sodamdadam.server.global.exception.SodamDadamException;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -54,8 +52,8 @@ public class JwtTokenProvider {
 
     }
 
-    public String resolveToken(HttpServletRequest request) {
-        String bearer = request.getHeader(JwtProperties.HEADER);
+    public String resolveToken(HttpServletRequest request, String tokenType) {
+        String bearer = request.getHeader(tokenType);
         return bearer;
     }
 
@@ -77,6 +75,26 @@ public class JwtTokenProvider {
             throw new SodamDadamException(ErrorCode.INVALID_JWT);
         }
     }
+
+    public boolean validateAccessToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(Base64.getEncoder().encodeToString(JwtProperties.SECRET_KEY.getBytes()))
+                    .parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            return true;
+        } catch (MalformedJwtException e) {
+            return false;
+        } catch (SignatureException e) {
+            return false;
+        } catch (UnsupportedJwtException e) {
+            return false;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
 
     public String getTokenSubject(String token) {
         return getTokenBody(token).getSubject();
