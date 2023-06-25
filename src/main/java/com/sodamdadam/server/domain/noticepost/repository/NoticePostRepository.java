213602c8c2979d8287dee32e4675fccf5f62a164
hyeonwook98 <hyeonwook98@naver.com>
2023-06-25 22:14:23 +0900

    feat: 공지글 조회 메서드 생성

--- src/main/java/com/sodamdadam/server/domain/noticepost/repository/NoticePostRepository.java
+++ src/main/java/com/sodamdadam/server/domain/noticepost/repository/NoticePostRepository.java
@@ -1,7 +1,10 @@
 package com.sodamdadam.server.domain.noticepost.repository;
 
 import com.sodamdadam.server.domain.noticepost.entity.NoticePost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticePostRepository extends JpaRepository<NoticePost, Long> {
+    Page<NoticePost> findAllByOrderByNoticeDate(Pageable pageable);
 }
