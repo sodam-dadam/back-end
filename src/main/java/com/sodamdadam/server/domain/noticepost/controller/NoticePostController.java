8f0961c2abdede34691ab69294d20108d55768fa
hyeonwook98 <hyeonwook98@naver.com>
2023-06-25 22:16:12 +0900

    feat: 공지글 조회 컨트롤러 추가

--- src/main/java/com/sodamdadam/server/domain/noticepost/controller/NoticePostController.java
+++ src/main/java/com/sodamdadam/server/domain/noticepost/controller/NoticePostController.java
@@ -1,17 +1,19 @@
 package com.sodamdadam.server.domain.noticepost.controller;
 
 import com.sodamdadam.server.domain.noticepost.dto.request.NoticePostRegisterRequest;
+import com.sodamdadam.server.domain.noticepost.service.NoticePostFindAllService;
 import com.sodamdadam.server.domain.noticepost.service.NoticePostRegisterService;
+import com.sodamdadam.server.global.dto.response.CommonResponse;
 import com.sodamdadam.server.global.dto.response.SuccessResponse;
 import com.sodamdadam.server.global.exception.ValidationException;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -23,6 +25,7 @@ import java.util.Map;
 public class NoticePostController {
 
     private final NoticePostRegisterService noticePostRegisterService;
+    private final NoticePostFindAllService noticePostFindAllService;
 
     @PostMapping("/register")
     public ResponseEntity<SuccessResponse> setNoticePost(@RequestBody @Valid NoticePostRegisterRequest request,
@@ -38,5 +41,10 @@ public class NoticePostController {
         }
 
         return noticePostRegisterService.setNoticePost(request);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<CommonResponse> getNoticePost(@PageableDefault(size = 10) Pageable pageable) {
+        return noticePostFindAllService.getNoticePosts(pageable);
     }
 }
