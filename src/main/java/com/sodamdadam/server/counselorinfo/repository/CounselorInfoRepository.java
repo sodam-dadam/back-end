78d2820677bf950834df18410fd2ce92cb5f9cfb
hyeonwook98 <hyeonwook98@naver.com>
2023-06-28 19:37:33 +0900

    feat: 상담전문가 소개 레포지토리 생성

---
+++ src/main/java/com/sodamdadam/server/counselorinfo/repository/CounselorInfoRepository.java
@@ -0,0 +1,8 @@
+package com.sodamdadam.server.counselorinfo.repository;
+
+import com.sodamdadam.server.counselorinfo.dto.response.CounselorInfoDto;
+
+public interface CounselorInfoRepository {
+
+    CounselorInfoDto getCounselorInfo();
+}
