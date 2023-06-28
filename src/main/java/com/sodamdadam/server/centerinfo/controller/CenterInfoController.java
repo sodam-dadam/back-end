a6005fc8e8bf3091d681b51072d60b4d1c961c0b
hyeonwook98 <hyeonwook98@naver.com>
2023-06-28 22:17:12 +0900

    refactor: 센터 정보 컨트롤러 수정

--- src/main/java/com/sodamdadam/server/centerinfo/controller/CenterInfoController.java
+++ src/main/java/com/sodamdadam/server/centerinfo/controller/CenterInfoController.java
@@ -1,10 +1,12 @@
 package com.sodamdadam.server.centerinfo.controller;
 
+import com.sodamdadam.server.centerinfo.dto.WayToComeDto;
 import com.sodamdadam.server.centerinfo.service.IntroductionService;
 import com.sodamdadam.server.centerinfo.service.ReservationInfoService;
 import com.sodamdadam.server.centerinfo.service.WayToComeService;
 import com.sodamdadam.server.global.dto.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +22,17 @@ public class CenterInfoController {
     private final ReservationInfoService reservationInfoService;
 
     @GetMapping("/waytocome")
-
     public ResponseEntity<CommonResponse> watToComeData() {
-        return wayToComeService.watToComeData();
+        WayToComeDto responseDto = wayToComeService.getWayToComeDto();
+
+        return new ResponseEntity<>(
+                CommonResponse.builder()
+                        .status(HttpStatus.OK.value())
+                        .message("WayToCome Response Data Success")
+                        .data(responseDto)
+                        .build(),
+                HttpStatus.valueOf(HttpStatus.OK.value())
+        );
     }
 
     @GetMapping("/introduction")
