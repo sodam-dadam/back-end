f7921ec77bb837c2275a5d688e442f38341cecb2
hyeonwook98 <hyeonwook98@naver.com>
2023-06-29 17:25:32 +0900

    feat: 집단상담 데이터 응답 메서드 구현

--- src/main/java/com/sodamdadam/server/counselinginfo/repository/CounselingInfoRepositoryImpl.java
+++ src/main/java/com/sodamdadam/server/counselinginfo/repository/CounselingInfoRepositoryImpl.java
@@ -20,9 +20,25 @@ public class CounselingInfoRepositoryImpl implements CounselingInfoRepository{
         return new CounselingInfoDto(mainText, imgUrl1, imgUrl2, subText1, subText2);
     }
 
+    private CounselingInfoDto createGroupCounselingInfo() {
+        List<String> mainText = Arrays.asList("집단의 역동을 활용하여', '개인관계, 대인관계를 돕는 집단상담");
+        String imgUrl1 = "../../../public/images/centerImg2.jpeg";
+        String imgUrl2 = "../../../public/images/centerImg2.jpeg";
+        List<String> subText1 = Arrays.asList("공동의 주제에 대해 나누는 성장,", "교육집단으로 서로의 경험과 지혜를", "나누는 시간을 갖습니다.");
+        List<String> subText2 = Arrays.asList("소담다담은 집단의 역동을 활용하여 자기 이해를 돕고,", "대인관계에서의 불편함이나 소외감,",
+                "갈등 요소들을 다루고 해결해감으로써", "개인의 성장을 이끌고 있습니다.");
+
+        return new CounselingInfoDto(mainText, imgUrl1, imgUrl2, subText1, subText2);
+    }
+
     @Override
     public CounselingInfoDto getPersonalCounselingInfo() {
         return createPersonalCounselingInfo();
+    }
+
+    @Override
+    public CounselingInfoDto getGroupCounselingInfo() {
+        return createGroupCounselingInfo();
     }
 
 }
