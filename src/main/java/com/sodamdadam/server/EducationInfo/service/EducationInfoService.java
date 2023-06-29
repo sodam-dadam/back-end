09d28dc204a8002a3b57eee4fc57cc8eb1edc2d9
hyeonwook98 <hyeonwook98@naver.com>
2023-06-29 20:35:50 +0900

    feat: 특강 및 워크숍 응답 서비스 구현

---
+++ src/main/java/com/sodamdadam/server/EducationInfo/service/EducationInfoService.java
@@ -0,0 +1,17 @@
+package com.sodamdadam.server.EducationInfo.service;
+
+import com.sodamdadam.server.EducationInfo.dto.response.EducationInfoDto;
+import com.sodamdadam.server.EducationInfo.repository.EducationInfoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class EducationInfoService {
+
+    private final EducationInfoRepository educationInfoRepository;
+
+    public EducationInfoDto getWorkShopEducationInfo() {
+        return educationInfoRepository.getWorkShopEducationInfo();
+    }
+}
