c17543fd601a821489fce652abf7e2dd8e87c9c9
hyeonwook98 <hyeonwook98@naver.com>
2023-06-29 17:01:38 +0900

    feat: 개인상담 응답 컨트롤러 추가

---
+++ src/main/java/com/sodamdadam/server/counselinginfo/controller/CounselingInfoController.java
@@ -0,0 +1,33 @@
+package com.sodamdadam.server.counselinginfo.controller;
+
+import com.sodamdadam.server.counselinginfo.dto.response.PersonalCounselingInfoDto;
+import com.sodamdadam.server.counselinginfo.repository.CounselingInfoRepository;
+import com.sodamdadam.server.global.dto.response.CommonResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping ("/counselinginfo")
+@RestController
+public class CounselingInfoController {
+
+    private final CounselingInfoRepository counselingInfoRepository;
+
+    @GetMapping("/personal")
+    public ResponseEntity<CommonResponse> getPersonalCounselingInfo() {
+        PersonalCounselingInfoDto responseDto = counselingInfoRepository.getPersonalCounselingInfo();
+
+        return new ResponseEntity<>(
+                CommonResponse.builder()
+                        .status(HttpStatus.OK.value())
+                        .message("PersonalCounselingInfo Response Data Success")
+                        .data(responseDto)
+                        .build(),
+                HttpStatus.valueOf(HttpStatus.OK.value())
+        );
+    }
+}
