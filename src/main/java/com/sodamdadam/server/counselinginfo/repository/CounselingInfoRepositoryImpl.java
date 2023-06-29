adb83aba257aac92c73e1c022798bd9dbe91419b
hyeonwook98 <hyeonwook98@naver.com>
2023-06-29 17:15:27 +0900

    fix: 공백 수정

--- src/main/java/com/sodamdadam/server/counselinginfo/repository/CounselingInfoRepositoryImpl.java
+++ src/main/java/com/sodamdadam/server/counselinginfo/repository/CounselingInfoRepositoryImpl.java
@@ -11,7 +11,7 @@ public class CounselingInfoRepositoryImpl implements CounselingInfoRepository{
 
     private PersonalCounselingInfoDto createPersonalCounselingInfo() {
         List<String> mainText = Arrays.asList("개인문제의 근원을 찾고", "지지와 공감 속에 함께 해결방향을 찾는 상담");
-        String imgUrl1 = "../../../public/images / centerImg2.jpeg";
+        String imgUrl1 = "../../../public/images/centerImg2.jpeg";
         String imgUrl2 = "../../../public/images/centerImg2.jpeg";
         List<String> subText1 = Arrays.asList("스트레스 완화, 적응력 향상을 위한", "1:1 성인 상담으로 다양한", "삶의 이슈와 정서를 다룹니다.");
         List<String> subText2 = Arrays.asList("삶의 고비마다 혼자서 감당하기 어려운 문제가 있고,", "때로는 심리적 고통을 겪기도 합니다.",
