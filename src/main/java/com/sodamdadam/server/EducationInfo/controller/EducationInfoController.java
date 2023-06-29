819e44bb7e419a745d19cda74f0473d7de0156a3
hyeonwook98 <hyeonwook98@naver.com>
2023-06-29 20:36:01 +0900

    feat: 특강 및 워크숍 응답 컨트롤러 구현

---
+++ src/main/java/com/sodamdadam/server/EducationInfo/controller/EducationInfoController.java
@@ -0,0 +1,33 @@
+package com.sodamdadam.server.EducationInfo.controller;
+
+import com.sodamdadam.server.EducationInfo.dto.response.EducationInfoDto;
+import com.sodamdadam.server.EducationInfo.service.EducationInfoService;
+import com.sodamdadam.server.global.dto.response.CommonResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/educations")
+@RestController
+public class EducationInfoController {
+
+    private final EducationInfoService educationInfoService;
+
+    @GetMapping("/workshops")
+    public ResponseEntity<CommonResponse> getWorkShopEducationInfo() {
+        EducationInfoDto responseDto = educationInfoService.getWorkShopEducationInfo();
+
+        return new ResponseEntity<>(
+                CommonResponse.builder()
+                        .status(HttpStatus.OK.value())
+                        .message("WorkShopEducationInfo Response Data Success")
+                        .data(responseDto)
+                        .build(),
+                HttpStatus.valueOf(HttpStatus.OK.value())
+        );
+    }
+}
