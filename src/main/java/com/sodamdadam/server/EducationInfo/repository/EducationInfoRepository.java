93f7046eb83187cb49091c5453ee771cf301d0b1
hyeonwook98 <hyeonwook98@naver.com>
2023-06-29 20:35:03 +0900

    feat: 특강 및 워크숍 응답 메서드 선언

---
+++ src/main/java/com/sodamdadam/server/EducationInfo/repository/EducationInfoRepository.java
@@ -0,0 +1,8 @@
+package com.sodamdadam.server.EducationInfo.repository;
+
+import com.sodamdadam.server.EducationInfo.dto.response.EducationInfoDto;
+
+public interface EducationInfoRepository {
+
+    EducationInfoDto getWorkShopEducationInfo();
+}
